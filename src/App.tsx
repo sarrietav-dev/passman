59b25e6eb8754240d3e15131b1a55a629a77fbb4
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-26 16:49:27 -0500

    Move changes to AddressDetails

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,11 @@
 import React from 'react';
+import { AddressDetails } from './components/AddressDetails';
 import './sass/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <div className="account-logo">A</div>
-        <h1 className="account-name">Account Name</h1>
-      </header>
-      <form action="" className="form">
-        <div className="form__field">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id=""
-            autoComplete="false"
-            required
-            placeholder="Name"
-          />
-        </div>
-
-        <div className="form__field">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            className="form__field"
-            id="password-field"
-            autoComplete="false"
-            required
-            placeholder="Password"
-          />
-          <button className="btn btn--password">
-            <i className="fa fa-eye"></i>
-          </button>
-          <button className="btn btn--password">
-            <i className="fa fa-refresh"></i>
-          </button>
-          <button className="btn btn--password">
-            <i className="fa fa-copy"></i>
-          </button>
-        </div>
-
-        <div className="form__field">
-          <label htmlFor="site-url">Site URL</label>
-          <input
-            type="url"
-            name="site-url"
-            id=""
-            autoComplete="false"
-            required
-            placeholder="Site URL"
-          />
-        </div>
-        <div className="form__field">
-          <label htmlFor="">Created at</label>
-          <p className="creation-date">{Date().toString()}</p>
-        </div>
-      </form>
-
-      <button className="btn btn--delete">
-        <i className="fa fa-trash"></i>
-        Delete
-      </button>
+      <AddressDetails />
     </div>
   );
 }

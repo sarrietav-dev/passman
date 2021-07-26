f331a12b3227eb468937ac5c4c2d65137df94b33
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-26 13:42:07 -0500

    Create markup for account page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,24 @@ import React from 'react';
 import './sass/main.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="account-logo">A</div>
+      <h1 className="account-name">Account Name</h1>
+      <form action="" className="form">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="" className="form__field" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="" className="form__field" />
+        <button className="btn">Generate</button>
+        <button className="btn">Copy</button>
+        <label htmlFor="site-url">Site URL</label>
+        <input type="url" name="site-url" id="" className="form__field" />
+        <label htmlFor="">Created at</label>
+        <p className="creation-date">{Date().toString()}</p>
+      </form>
+    </div>
+  );
 }
 
 export default App;

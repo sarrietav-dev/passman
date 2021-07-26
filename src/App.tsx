0562eb205d08b3db34007f59c5a80166d94c64e9
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-26 14:12:36 -0500

    Add styles to the input elements

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,30 @@ function App() {
         <h1 className="account-name">Account Name</h1>
       </header>
       <form action="" className="form">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="" className="form__field" />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="" className="form__field" />
-        <button className="btn">Generate</button>
-        <button className="btn">Copy</button>
-        <label htmlFor="site-url">Site URL</label>
-        <input type="url" name="site-url" id="" className="form__field" />
-        <label htmlFor="">Created at</label>
-        <p className="creation-date">{Date().toString()}</p>
+        <div className="form__field">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="" />
+        </div>
+
+        <div className="form__field">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id=""
+            className="form__field"
+          />
+          <button className="btn">Generate</button>
+          <button className="btn">Copy</button>
+        </div>
+        <div className="form__field">
+          <label htmlFor="site-url">Site URL</label>
+          <input type="url" name="site-url" id="" />
+        </div>
+        <div className="form__field">
+          <label htmlFor="">Created at</label>
+          <p className="creation-date">{Date().toString()}</p>
+        </div>
       </form>
     </div>
   );

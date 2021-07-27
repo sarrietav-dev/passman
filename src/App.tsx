6239e13ac5170a6485638545f152ad8816fcae17
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-27 01:28:14 -0500

    Put imageUrl to dummy values

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,27 @@ function App() {
           <SearchBar />
           <CreateButton />
         </header>
-        <VaultItem title="Facebook" username="sarrietav" active />
-        <VaultItem title="Whatsapp" username="sarrietav@gmail.com" />
-        <VaultItem title="Instagram" username="sarrietav@protonmail.com" />
+        <VaultItem
+          title="Facebook"
+          username="sarrietav"
+          active
+          imageUrl="https://static.xx.fbcdn.net/rsrc.php/v3/yN/r/EWLVhDVJTum.png"
+        />
+        <VaultItem
+          title="Whatsapp"
+          username="sarrietav@gmail.com"
+          imageUrl="https://www.whatsapp.com/apple-touch-icon-precomposed.png"
+        />
+        <VaultItem
+          title="Instagram"
+          username="sarrietav@protonmail.com"
+          imageUrl="https://www.instagram.com/static/images/ico/apple-touch-icon-180x180-precomposed.png/c06fdb2357bd.png"
+        />
+        <VaultItem
+          title="Google Drive"
+          username="sarrietav@unicartagena.edu.co"
+          imageUrl="https://ssl.gstatic.com/images/branding/product/2x/hh_drive_96dp.png"
+        />
         <VaultItem title="College" username="sarrietav@unicartagena.edu.co" />
       </nav>
       <AddressDetails />

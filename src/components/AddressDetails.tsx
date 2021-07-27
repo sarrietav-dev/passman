3eaad5899d0c5b3034262c40407173b3a88bab17
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-26 20:47:17 -0500

    Focus on input when the wrapper is clicked

--- src/components/AddressDetails.tsx
+++ src/components/AddressDetails.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { RefObject, useRef } from 'react';
 import './address-details.scss';
 import '../sass/button.scss';
 
 export const AddressDetails = () => {
+  const nameFieldRef = useRef<HTMLInputElement>(null);
+  const passwordFieldRef = useRef<HTMLInputElement>(null);
+  const urlFieldRef = useRef<HTMLInputElement>(null);
+
+  const focusInput = (ref: RefObject<HTMLInputElement>) => {
+    ref.current?.focus();
+  };
+
   return (
     <div className="address-details">
       <div className="action-buttons">
@@ -21,12 +29,22 @@ export const AddressDetails = () => {
       </header>
       <hr />
       <form action="" className="form">
-        <div className="form__field">
+        <div className="form__field" onClick={() => focusInput(nameFieldRef)}>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="" autoComplete="false" required />
+          <input
+            type="text"
+            name="name"
+            id=""
+            autoComplete="false"
+            required
+            ref={nameFieldRef}
+          />
         </div>
 
-        <div className="form__field form__field--pass">
+        <div
+          className="form__field form__field--pass"
+          onClick={() => focusInput(passwordFieldRef)}
+        >
           <div className="password-field-wrapper">
             <label htmlFor="password">Password</label>
             <input
@@ -36,6 +54,7 @@ export const AddressDetails = () => {
               id="password-field"
               autoComplete="false"
               required
+              ref={passwordFieldRef}
             />
           </div>
           <div className="password-buttons">
@@ -51,7 +70,7 @@ export const AddressDetails = () => {
           </div>
         </div>
 
-        <div className="form__field">
+        <div className="form__field" onClick={() => focusInput(urlFieldRef)}>
           <label htmlFor="site-url">Site URL</label>
           <input
             type="url"
@@ -59,6 +78,7 @@ export const AddressDetails = () => {
             id=""
             autoComplete="false"
             required
+            ref={urlFieldRef}
           />
         </div>
         <div className="form__field">

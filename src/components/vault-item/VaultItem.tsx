881864a4e04969f10d8b4cacde6d23997dc88533
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-27 00:11:28 -0500

    Create VaultItem component

---
+++ src/components/vault-item/VaultItem.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './vault-item.scss';
+
+interface VaultItemProps {
+  active: boolean;
+  imageUrl: string;
+  title: string;
+  username: string;
+}
+
+export const VaultItem = ({
+  active,
+  imageUrl,
+  title,
+  username,
+}: VaultItemProps) => {
+  return (
+    <div className={`vault-item ${active ? 'vault-item--active' : ''}`}>
+      <div className="vault-item__image">
+        <img
+          src={imageUrl}
+          alt="Vault item logo. Probably a logo of a company."
+        />
+      </div>
+      <div className="vault-item__info">
+        <div className="vault-item__title">{title}</div>
+        <div className="vault-item__username">{username}</div>
+      </div>
+    </div>
+  );
+};

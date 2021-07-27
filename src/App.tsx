9965bb3ad98918bf58e49b2ca54624c625475b60
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-26 21:13:04 -0500

    Add search-bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { AddressDetails } from './components/AddressDetails';
 import './App.scss';
+import { SearchBar } from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <div className="nav">abc</div>
+      <div className="nav">
+        <SearchBar />
+      </div>
       <AddressDetails />
     </div>
   );
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './search-bar.scss';
+
+export const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <i className="fa fa-search"></i>
+      <input type="text" name="" id="" placeholder="Search password" />
+    </div>
+  );
+};

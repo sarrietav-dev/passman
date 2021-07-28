3c293598af109a632edafa8390692e319423f675
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-27 20:48:41 -0500

    style(app.tsx): delete unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { AddressDetails } from './components/address-details/AddressDetails';
 import './App.scss';
 
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
     dispatch(fetchItems());
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="App">

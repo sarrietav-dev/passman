9f754ea7b7613176dd38eb98137a96bc2cf726bc
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-28 10:42:36 -0500

    Checking if url is valid

--- src/components/address-details/AddressDetails.tsx
+++ src/components/address-details/AddressDetails.tsx
@@ -1,57 +1,94 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { FormEvent, RefObject, useRef, useState } from 'react';
 import './address-details.scss';
 import '../../sass/button.scss';
 import { AddressDetailsForm } from './AddressDetailsForm';
 import { v4 as uuid } from 'uuid';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { patchItem, postItem } from '../../store/thunks/app-thunks';
+import axios from 'axios';
 
 export const AddressDetails = () => {
   const { currentItem } = useAppSelector((state) => state.app);
   const { editingMode } = useAppSelector((state) => state.form);
   const dispatch = useAppDispatch();
 
-  const onSubmit = (e: any) => {
-    const [id, account_name, username, password, url, logoUrl, created_at] = [
-      uuid(),
-      e.target['0'].value,
-      e.target['1'].value,
-      e.target['2'].value,
-      e.target['6'].value,
-      e.target['7'].value,
-      Date(),
-    ];
+  const refs: {
+    [key: string]: RefObject<HTMLInputElement>;
+  } = {
+    nameFieldRef: useRef<HTMLInputElement>(null),
+    usernameFieldRef: useRef<HTMLInputElement>(null),
+    passwordFieldRef: useRef<HTMLInputElement>(null),
+    urlFieldRef: useRef<HTMLInputElement>(null),
+    logoFieldRef: useRef<HTMLInputElement>(null),
+  };
 
-    if (currentItem === undefined) {
-      dispatch(
-        postItem({
-          account_name: account_name as string,
-          id,
-          created_at,
-          username: username as string,
-          password: password as string,
-          site_url: url as string,
-          logo_url: logoUrl as string,
-        }),
-      );
-    } else {
-      dispatch(
-        patchItem({
-          account_name: account_name as string,
-          id: currentItem.id,
-          created_at,
-          username: username as string,
-          password: password as string,
-          site_url: url as string,
-          logo_url: logoUrl as string,
-        }),
-      );
+  const onSubmit = (e: FormEvent) => {
+    Object.keys(refs).forEach((refKey) => {
+      const ref = refs[refKey].current;
+      if (ref?.classList.contains('form__field--invalid'))
+        ref?.classList.remove('form__field--invalid');
+      ref?.setCustomValidity('');
+    });
+
+    const areFilled = Object.keys(refs).filter(
+      (ref) =>
+        refs[ref].current?.value === '' ||
+        refs[ref].current?.value === undefined,
+    );
+
+    if (areFilled.length !== 0) {
+      e.preventDefault();
+      areFilled.forEach((key) => {
+        refs[key].current?.classList.add('form__field--invalid');
+        refs[key].current?.setCustomValidity('Please fill out this field');
+      });
+      return false;
     }
+
+    axios
+      .get(refs.urlFieldRef.current!.value)
+      .then((response) => {
+        if (response.status > 400) {
+          e.preventDefault();
+          refs.ulrFieldRef.current?.classList.add('form__field--invalid');
+          refs.urlFieldRef.current?.setCustomValidity(
+            'This url does not exist',
+          );
+          throw Error();
+        }
+      })
+      .then(() => {
+        if (currentItem === undefined) {
+          dispatch(
+            postItem({
+              id: uuid(),
+              created_at: Date(),
+              account_name: refs.nameFieldRef.current!.value,
+              username: refs.usernameFieldRef.current!.value,
+              password: refs.passwordFieldRef.current!.value,
+              site_url: refs.urlFieldRef.current!.value,
+              logo_url: refs.logoFieldRef.current!.value,
+            }),
+          );
+        } else {
+          dispatch(
+            patchItem({
+              id: currentItem.id,
+              created_at: Date(),
+              account_name: refs.nameFieldRef.current!.value,
+              username: refs.usernameFieldRef.current!.value,
+              password: refs.passwordFieldRef.current!.value,
+              site_url: refs.urlFieldRef.current!.value,
+              logo_url: refs.logoFieldRef.current!.value,
+            }),
+          );
+        }
+      })
+      .catch((error) => console.error(error));
   };
 
   const getFormParent = () => {
-    const child = <AddressDetailsForm />;
+    const child = <AddressDetailsForm refs={refs} />;
 
     return editingMode ? (
       <form className="form" id="form" onSubmit={onSubmit}>

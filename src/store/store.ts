3911a1a03a4f474dae9e33dbba0c5affb048d70b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-21 18:05:43 -0500

    style(store): add whitespace

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,7 @@ import appReducer from './reducers/App.reducer';
 import { configureStore } from '@reduxjs/toolkit'; // ...
 import formReducer from './reducers/Form.reducer';
 import navbarReducer from './reducers/Navbar.reducer';
+
 export const store = configureStore({
   reducer: { app: appReducer, form: formReducer, navbar: navbarReducer },
 });

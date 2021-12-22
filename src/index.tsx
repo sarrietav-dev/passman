a7ed2688c5f47a91373111d6c7bd8b853139f094
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-21 22:52:25 -0500

    Configure tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store/store';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>

339c443e8ece5e28cb81a21bb5d523275a190682
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-22 22:49:45 -0500

    Refactor component return type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,11 +5,15 @@ interface ButtonProps {
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Button: React.FunctionComponent<ButtonProps> = ({
-  children,
-  className,
-  onClick,
-}) => (
+type ComponentType = React.FunctionComponent<
+  ButtonProps &
+    React.DetailedHTMLProps<
+      React.ButtonHTMLAttributes<HTMLButtonElement>,
+      HTMLButtonElement
+    >
+>;
+
+export const Button: ComponentType = ({ children, className, onClick }) => (
   <button
     onClick={onClick}
     className={`text-white font-roboto text-sm mx-1 my-0 border-none rounded bg-black bg-opacity-20 hover:bg-white hover:bg-opacity-50 active:bg-white active:bg-opacity-30 ${className}`}

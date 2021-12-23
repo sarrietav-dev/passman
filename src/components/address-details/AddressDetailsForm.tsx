8040ad160d8efe5e7e6f8da41197d157a74e5d5c
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-22 19:22:54 -0500

    Extract FormField components

--- src/components/address-details/AddressDetailsForm.tsx
+++ src/components/address-details/AddressDetailsForm.tsx
@@ -1,17 +1,17 @@
 import { RefObject, useState } from 'react';
-import { Img } from '../Img';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { switchEditingMode } from '../../store/reducers/Form.reducer';
 import { deleteItem } from '../../store/thunks/app-thunks';
 import FormField from './components/FormField';
-import {
-  ShowPasswordButton,
-  GeneratePasswordButton,
-  CopyToClipboardButton,
-} from './components/password-buttons';
 import { VaultItem } from '../../types/types';
 import { AnyAction, Dispatch, ThunkDispatch } from '@reduxjs/toolkit';
-import { FormInput } from './components/FormInput';
+import {
+  AccountNameField,
+  LogoUrlField,
+  PasswordField,
+  SiteUrlField,
+  UsernameField,
+} from './components/form-fields';
 
 export interface AddressDetailState {
   name: string | undefined;
@@ -34,125 +34,51 @@ export const AddressDetailsForm = ({ refs }: AddressDetailsProps) => {
     showPassword: false,
   });
 
-  function generatePassword() {
-    var length = 12,
-      charset =
-        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-      retVal = '';
-    for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
-  }
-
-  const ItemLogo = () =>
-    isLogoAvailable() ? (
-      <Img url={currentItem?.logo_url} title={getLogoText()} />
-    ) : (
-      <>getLogoText()</>
-    );
-
-  const isLogoAvailable = () =>
-    currentItem?.logo_url !== '' || currentItem !== undefined;
-
-  const getLogoText = () => {
-    if (currentItem) return currentItem?.account_name.charAt(0).toUpperCase();
-    return state.name === undefined || state.name === ''
-      ? '@'
-      : state.name.charAt(0).toUpperCase();
-  };
-
   return (
     <>
-      <FormField
-        className="flex place-content-center font-roboto"
+      <AccountNameField
+        state={state}
         editing={editingMode}
         ref={refs.nameFieldRef}
-      >
-        <span className="text-3xl text-gray-400 w-16 h-16 text-center rounded inline-flex place-items-center">
-          <ItemLogo />
-        </span>
-        <FormInput
-          type="text"
-          placeholder="Account name"
-          className="text-3xl ml-5 w-4/5"
-          editing={editingMode}
-          ref={refs.nameFieldRef}
-          currentItem={currentItem?.account_name ?? ''}
-          onChange={(e) => {
-            if (e.target.value !== '')
-              setState({ ...state, name: e.target.value });
-          }}
-        />
-      </FormField>
+        currentItem={currentItem}
+        currentItemName={currentItem?.account_name}
+        setState={setState}
+      />
+
       <hr className="mx-0 my-5" />
-      <FormField editing={editingMode} ref={refs.usernameFieldRef}>
-        <label htmlFor="username">Username</label>
-        <FormInput
-          type="text"
-          name="username"
-          autoComplete="false"
-          ref={refs.usernameFieldRef}
-          editing={editingMode}
-          currentItem={currentItem?.username ?? ''}
-        />
-      </FormField>
-      <FormField
-        className="flex items-center"
+
+      <UsernameField
+        ref={refs.usernameFieldRef}
+        editing={editingMode}
+        currentItemName={currentItem?.username}
+      />
+
+      <PasswordField
         editing={editingMode}
         ref={refs.passwordFieldRef}
-      >
-        <div className="basis-4/5">
-          <label htmlFor="password">Password</label>
-          <FormInput
-            name="password"
-            id="password-field"
-            autoComplete="false"
-            className="p-4 inline-block rounded transition"
-            editing={editingMode}
-            ref={refs.passwordFieldRef}
-            currentItem={currentItem?.password ?? ''}
-            type={state.showPassword ? 'text' : 'password'}
-          />
-        </div>
-        <div>
-          <ShowPasswordButton state={state} setState={setState} />
-          {editingMode && (
-            <GeneratePasswordButton
-              ref={refs.passwordFieldRef}
-              generatePassword={generatePassword}
-            />
-          )}
-          <CopyToClipboardButton ref={refs.passwordFieldRef} />
-        </div>
-      </FormField>
-      <FormField editing={editingMode} ref={refs.urlFieldRef}>
-        <label htmlFor="site-url">Site URL</label>
-        <FormInput
-          type="url"
-          name="site-url"
-          autoComplete="false"
-          ref={refs.urlFieldRef}
-          editing={editingMode}
-          disabled={!editingMode}
-          currentItem={currentItem?.site_url ?? ''}
-        />
-      </FormField>
-      <FormField editing={editingMode} ref={refs.urlFieldRef}>
-        <label htmlFor="site-url">Logo URL</label>
-        <FormInput
-          type="url"
-          name="site-url"
-          autoComplete="false"
-          editing={editingMode}
-          ref={refs.logoFieldRef}
-          currentItem={currentItem?.logo_url ?? ''}
-        />
-      </FormField>
+        currentItemName={currentItem?.password}
+        showPassword={state.showPassword}
+        setState={setState}
+        state={state}
+      />
+
+      <SiteUrlField
+        ref={refs.urlFieldRef}
+        editing={editingMode}
+        currentItemName={currentItem?.site_url}
+      />
+
+      <LogoUrlField
+        editing={editingMode}
+        ref={refs.logoFieldRef}
+        currentItemName={currentItem?.logo_url ?? ''}
+      />
+
       <FormField>
         <label>Created at</label>
         <p>{currentItem?.created_at}</p>
       </FormField>
+
       <div className="flex justify-end items-stretch">
         <SubmitButton dispatch={dispatch} editing={editingMode} />
         {currentItem !== undefined && (
